0bac80e5a4e0e20068c3dfbc1e9d9660de393697
sayyedyasin0612 <sayyedyasin669@gmail.com>
2025-11-05 09:56:34 +0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,48 @@
+// var a=10
+// var b=50
+
+// c=a
+// a=b
+// b=c
+// console.log(a);
+// console.log(b);
+// let username = "Yaseen";
+// let dispname = username || "Guest";
+// console.log(dispname);
+
+// let day = "sunday";
+// if (day === "saturday" || day === "sunday")
+//     console.log("week Ends");
+// else
+//     console.log("week Days");
+
+// let isHot = true;
+// console.log(isHot);
+// console.log(!isHot);
+
+// let login = true;
+// if (!login)
+//     console.log("login frist...", login);
+// else
+//     console.log("logged In...", login);
+
+// console.log(!!"Welcome"); // true bcoz non-empty string
+// console.log(!!0); // false
+// console.log(!!null);  // false
+// console.log(!!123); // true
+
+// let money=false;
+// let card=false;
+// if(!(money || card))
+//     console.log("You can't buy anything! ");
+// else
+//     console.log("can buy...");
+
+
+
+    
+
+
+    
+    
+    
